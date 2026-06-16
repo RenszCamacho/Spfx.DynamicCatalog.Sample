e6a20fb470fa02c618e554f6e18d286a8e9b452b
Renszo Camacho <varsovia84@gmail.com>
2026-06-16 13:25:13 +0200

    refactor: extract shared hooks, utils, and helpers for SRP compliance
    - Add src/hooks/useDynamicPropertySubscription.ts (reusable DynamicProperty subscription)
    - Add src/utils/state-helpers.ts (toggleItem, cycleInStock, handleResult)
    - Add src/helpers/filter-criteria.ts (buildFilterCriteria, isSelectedInProducts)
    - Update barrel exports in utils/index.ts and helpers/index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
 export { mapToProduct, mapToProducts } from './mappers';
 export { buildFilters } from './odata-filters';
+export { buildFilterCriteria, isSelectedInProducts } from './filter-criteria';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { ok, err, mapResult, flatMapResult } from './result';
+export { toggleItem, cycleInStock, handleResult } from './state-helpers';
